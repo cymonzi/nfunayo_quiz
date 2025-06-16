9af668576b2a116a03bae96611a5922b94eb254a
cymonzi <cymonmusinguzi@gmail.com>
2025-06-17 00:34:36 +0300

    update vercel-build.sh to fix git safe directory

--- vercel-build.sh
+++ vercel-build.sh
@@ -4,7 +4,13 @@ echo "Downloading Flutter..."
 curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.6-stable.tar.xz
 echo "Extracting Flutter..."
 tar xf flutter_linux_3.19.6-stable.tar.xz
+
+# Add Flutter to PATH
 export PATH="$PWD/flutter/bin:$PATH"
+
+# Fix: Allow git to use the Flutter directory
+git config --global --add safe.directory "$PWD/flutter"
+
 echo "Flutter version:"
 flutter --version
 echo "Enabling web..."
